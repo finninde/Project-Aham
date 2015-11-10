67889fba31d347d1c4f8c9f8f2af6a0cd5235718
n35n0m <n35n0m@gmail.com>
2015-11-10 14:29:10 +0100

    Datafile added, mapdatafile created with method and class headers

---
+++ mapdata.py
@@ -0,0 +1,51 @@
+##File that treats all theoretical aspects of map data, not the visualization of it.
+## CONTENTS
+## - Help method Dijkstras Algorithm
+## - Help method Travelling Salesman Problem
+## - Class Node
+## - Class Edge
+## - Class UndirectedGraph
+
+
+def dijkstra():
+    pass
+
+def salesman():
+    pass
+
+class Node():
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+class Edge():
+    def __init__(self):
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def get_distance(self):
+        pass
+
+    def get_property(self):
+        pass
+
+
+class UndirectedGraph():
+    def __init__(self):
+        pass
+
+    def insert_node(self, node):
+        pass
+
+    def delete_node(self, node):
