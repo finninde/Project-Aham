fbef4325f1a1c9961cf4fd17fe30206d789cae90
Geir lanepc <Geirsvenn@dhcp-010096.wlan.ntnu.no>
2015-11-11 14:38:15 +0100

    Initialize graph with xml wrapper

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from edge import Edge
 from undirectedgraph import UndirectedGraph
 from dijkstra import dijkstra
 from salesman import salesman
-
+from mapparser import Mapparser
 
 
 if __name__ == "__main__":
+    mpp = Mapparser('roads-approx.xml')
+    waypoints = mpp.getWaypoints()
+    patches = mpp.getPatches()
+    # Node is waypoints
     graph = UndirectedGraph()
-    graph.insert_node(Node(1,2))
-    graph.insert_node(Node(1,2))
+    for node in waypoints:
+        graph.insert_node(Node(node[0],node[1]))
+    for edge in patches:
+        graph.insert_edge(Edge((edge[0], edge[1]), (edge[2], edge[3]), edge[4]))
 
     salesman(graph)
 
