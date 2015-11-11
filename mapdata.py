91ff88243fb96b78e6791e0702f69cd8feba549c
n35n0m <n35n0m@gmail.com>
2015-11-11 12:29:03 +0100

    Dijkstra implemented, needs debug (probably dict key errors)

--- mapdata.py
+++ mapdata.py
@@ -50,20 +50,31 @@ def dijkstra(undirectedgraph, sourcenode):
         raise KeyError('pop from an empty priority queue')
     ## CODE FROM https://docs.python.org/2/library/heapq.html END
 
+    nodedata = {}
     for node in undirectedgraph.nodes:
+        nodedata[node] = [float("inf"), None] # Distance from source + parent attributes
         add_task(node, float("inf"))
 
     add_task(sourcenode, 0)         #Change priority of source node to 0. We wish to start with the source node.
 
     while Q is not None:            #While there are nodes in the min-priority queue
         ##PSEUDOCODE START
-        u = pop_task() #extract-min(Q)          #Get the node with the lowest priority/distance
-        S = S union u
+        u = pop_task() #extract-min(Q)           #Get the node with the lowest priority/distance
+        S.append([u])                              #Get the intersection of the set of shortest path vertices and u (append u)
 
-        for vertex in G.adj[]:
-            relax(u,v,w)
+        for v in undirectedgraph.edges[u.latitude, u.longitude]:    ## Should be Key (endnode) which we use to get Edgedata (see class UndirectedGraph)
+            # Relax each edge! Remember to update each priority in the minqueue by calling add_task(task, priority)
+            # Do we need to remember parent? Seems like we only need to remember source (first element in S for this task?
 
-        ##PSEUDOCODE END
+            w = undirectedgraph[(u.latitude, u.longitude)][(v.latitude, v.longitude)].weight
+            #u.d is found above
+            v.d = entry_finder[v][0]    #Should give us priority, and thus current weight
+
+            if v.d > u.d + w:
+                add_task(v, (u.d+w))        #Update the priority queue
+                nodedata[v][0] = u.d + w    #Update the distance from source as well
+                nodedata[v][1] = u          #Update the parent!
+
 
 
 
