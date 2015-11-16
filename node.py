bd10021d527c63dfbc9ae2e8fdf7eee37ac7d4d1
n35n0m <n35n0m@gmail.com>
2015-11-16 10:53:41 +0100

    Polished function, removed uneccesarry comments and added formatting

--- node.py
+++ node.py
@@ -1,19 +1,17 @@
 ## Representation of a node (road intersection on a map)
 class Node():
     def __init__(self, latitude, longitude):
-        self.latitude = latitude        # "x-coordinate"
-        self.longitude = longitude      # "y-coordinate"
+        self.latitude = latitude
+        self.longitude = longitude
 
-    ## Defines how the compiler should check for equality of two instances
+    # How to check if two nodes are equal
     def __eq__(self, other):
-        #If coordinates are an exact match (currently no leeway), we assume the nodes to be identical
         if self.latitude == other.latitude and self.longitude == other.longitude:
             return True
-        #Elsewise, we assume the nodes (road intersections) to be different
         else:
             return False
 
-    ## Defines how the compiler should display the instance contents as a string
+    ## How to print Node instance to console
     def __repr__(self):
         theString = "NODEDATA\n" + "Latitude:\t" + str(self.latitude) +  "\t\tLongitude:\t" + str(self.longitude) + "\n\n"
         return theString
